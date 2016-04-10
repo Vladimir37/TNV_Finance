5410fc4d73e3d6cb8e6c09ebaf541483970b8488
Vladimir37 <vladimir_zapas@mail.ru>
2016-04-10 20:35:09 +0000

    Navbar

---
+++ front/static/js/ng/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('TNV', []);
+
+app.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[');
+  $interpolateProvider.endSymbol(']}');
+}]);
